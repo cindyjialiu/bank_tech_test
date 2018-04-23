e3a9a93b382a73a23b890f6f55f429aa09a4770f
Cindy <jl7e12@gmail.com>
2018-04-23 12:07:20 +0100

    Test1: deposit passed(when the original balance is 0)

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,11 +1,23 @@
-# require 'bank_account'
-#
-# describe BankAccount do
-#
-#   descirbe '#deposit' do
-#     bank_account = BankAccount.new
-#     expect(bank_account.deposit(amount))
-#
-#   end
-#
-# end
+require 'bank_account'
+
+describe BankAccount do
+
+  describe '#deposit' do
+    context 'when the original balance is 0' do
+      it 'returns the new balance of 1000.00' do
+        bank_account = BankAccount.new
+        bank_account.deposit(1000.00)
+        expect(bank_account.balance).to eq(1000.00)
+      end
+    end
+
+    context 'when the original balance is 1000' do
+      it 'returns the new balance of 2000.00' do
+        bank_account = BankAccount.new(1000)
+        bank_account.deposit(1000.00)
+        expect(bank_account.balance).to eq(2000.00)
+      end
+    end
+  end
+
+end
