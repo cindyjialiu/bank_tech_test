b95d2ad9dffe489d9dd3e46283658f2fce69358a
Cindy <jl7e12@gmail.com>
2018-04-23 14:33:18 +0100

    Added date in deposit method and test passed

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,17 +1,33 @@
+require 'date'
+
 class BankAccount
-  attr_reader :balance
+  attr_reader :balance, :date
 
   def initialize(balance = 0)
+    @date = get_date
+    @credit = 0
+    @debit = 0
     @balance = balance
   end
 
   def deposit(amount)
-    (@balance += amount).to_f
+    {
+      @date => {
+        credit: amount.to_f,
+        debit: @debit,
+        balance: (@balance += amount).to_f
+      }
+    }
   end
 
   def withdrawal(amount)
     raise 'Sorry, your balance is too low.' if @balance < amount
     (@balance -= amount).to_f
+  end
+
+  private
+  def get_date
+    DateTime.now.strftime("%d/%m/%Y").to_sym
   end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -6,16 +6,28 @@ describe BankAccount do
     context 'when the original balance is 0' do
       it 'returns the new balance 1000.00' do
         bank_account = BankAccount.new
-        bank_account.deposit(1000)
-        expect(bank_account.balance).to eq(1000.00)
+        expect(bank_account.deposit(1000)).to eq({
+           bank_account.date => {
+             credit: 1000.00,
+             debit: 0,
+             balance: 1000.00
+            }
+          }
+      )
       end
     end
 
     context 'when the original balance is 1000' do
       it 'returns the new balance 2000.00' do
         bank_account = BankAccount.new(1000)
-        bank_account.deposit(1000)
-        expect(bank_account.balance).to eq(2000.00)
+        expect(bank_account.deposit(1000)).to eq({
+           bank_account.date => {
+             credit: 1000.00,
+             debit: 0,
+             balance: 2000.00
+            }
+          }
+      )
       end
     end
   end
