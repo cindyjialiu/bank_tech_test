b29052744eb820310712b4e74ac4076ac3721907
Cindy <jl7e12@gmail.com>
2018-04-23 23:22:32 +0100

    Removed test with the initial balance is not 0 as it's unnecessary

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,39 +1,30 @@
 require 'date'
 
 class BankAccount
-  attr_reader :balance, :date
+  attr_accessor :balance, :date, :history
 
-  def initialize(balance = 0)
-    @date = get_date
-    @credit = 0
-    @debit = 0
-    @balance = balance
+  def initialize
+    @amount = 0
+    @balance = 0
+    @history = []
   end
 
-  def deposit(amount)
-    {
-      @date => {
-        credit: amount.to_f,
-        debit: @debit.to_f,
-        balance: (@balance += amount).to_f
-      }
+  def deposit(amount, date)
+    transaction = {
+      date: date,
+      amount: amount,
+      balance: @balance += amount
     }
+    @history << transaction
   end
 
-  def withdrawal(amount)
+  def withdrawal(amount, date)
     raise 'Sorry, your balance is too low.' if @balance < amount
-    {
-      @date => {
-        credit: @credit.to_f,
-        debit: amount.to_f,
-        balance: (@balance -= amount).to_f
-      }
+    transaction = {
+      date: date,
+      amount: -amount,
+      balance: @balance -= amount
     }
+    @history << transaction
   end
-
-  private
-  def get_date
-    DateTime.now.strftime("%d/%m/%Y").to_sym
-  end
-
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,33 +1,20 @@
 require 'bank_account'
 
 describe BankAccount do
+  subject(:bank_account) { described_class.new }
+  date = Date.new(2018, 4, 23)
 
   describe '#deposit' do
     context 'when the original balance is 0' do
-      it 'returns the new balance 1000.00' do
-        bank_account = BankAccount.new
-        expect(bank_account.deposit(1000)).to eq({
-           bank_account.date => {
-             credit: 1000.00,
-             debit: 0,
-             balance: 1000.00
-            }
+      it 'returns the new balance 1000.' do
+        expected = [
+          {
+            date: date,
+            amount: 1000,
+            balance: 1000
           }
-      )
-      end
-    end
-
-    context 'when the original balance is 1000' do
-      it 'returns the new balance 2000.00' do
-        bank_account = BankAccount.new(1000)
-        expect(bank_account.deposit(1000)).to eq({
-           bank_account.date => {
-             credit: 1000.00,
-             debit: 0,
-             balance: 2000.00
-            }
-          }
-      )
+        ]
+        expect(bank_account.deposit(1000, date)).to eq expected
       end
     end
   end
@@ -35,24 +22,23 @@ describe BankAccount do
   describe '#withdrawal' do
     context 'when the original balance is >= withdrawal amount' do
       it 'returns the new balance 0' do
-        bank_account = BankAccount.new(1000)
-        expect(bank_account.withdrawal(500)).to eq({
-           bank_account.date => {
-             credit: 0,
-             debit: 500.00,
-             balance: 500.00
-            }
+        bank_account.balance = 1000
+        expected = [
+          {
+            date: date,
+            amount: -500,
+            balance: 500
           }
-      )
+        ]
+        expect(bank_account.withdrawal(500, date)).to eq expected
       end
     end
 
     context 'when the original balance is 0' do
       it 'should raise an error' do
-        bank_account = BankAccount.new
-        expect { bank_account.withdrawal(1000) }.to raise_error('Sorry, your balance is too low.')
+        error_message = 'Sorry, your balance is too low.'
+        expect { bank_account.withdrawal(1000, date) }.to raise_error(error_message)
       end
     end
   end
-
 end
