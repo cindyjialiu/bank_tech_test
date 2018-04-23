faad0efa0bc040cbfbe481713eeb0d12409cc49a
Cindy <jl7e12@gmail.com>
2018-04-23 12:07:20 +0100

    Test1: deposit passed

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,11 +1,13 @@
-# require 'bank_account'
-#
-# describe BankAccount do
-#
-#   descirbe '#deposit' do
-#     bank_account = BankAccount.new
-#     expect(bank_account.deposit(amount))
-#
-#   end
-#
-# end
+require 'bank_account'
+
+describe BankAccount do
+
+  describe '#deposit' do
+    it 'returns the new balance of the deposit amount plus original balance' do
+      bank_account = BankAccount.new
+      bank_account.deposit(1000.00)
+      expect(bank_account.balance).to eq(1000.00)
+  end
+end
+
+end
