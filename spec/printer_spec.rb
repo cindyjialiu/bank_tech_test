d9890e570343bc6a39ea76cd2d5b48a24d4df6e8
Cindy <jl7e12@gmail.com>
2018-04-24 14:50:18 +0100

    Minor change of print test, add \n after statement

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -8,8 +8,8 @@ describe Printer do
       t1 = '23/04/2018 || || 500.00 || 1500.00'
       t2 = '23/04/2018 || 1000.00 || || 2000.00'
       t3 = '23/04/2018 || 2000.00 || || 1000.00'
-      statement = title + "\n" + t3 + "\n" + t2 + "\n" + t1 + "\n"
-      expect { printer.print(statement) }.to output(statement).to_stdout
+      statement = title + "\n" + t3 + "\n" + t2 + "\n" + t1
+      expect { printer.print(statement) }.to output(statement + "\n").to_stdout
     end
   end
 end
