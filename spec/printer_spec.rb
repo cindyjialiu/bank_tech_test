8931d848c181e43ed5ef0269883d20ea41cf0026
Cindy <jl7e12@gmail.com>
2018-04-24 14:54:34 +0100

    Added one more test for print method

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -2,14 +2,17 @@ require 'printer'
 
 describe Printer do
   describe '#print' do
+    printer = Printer.new
+    title = 'date || credit || debit || balance'
+    t1 = '23/04/2018 || || 500.00 || 1500.00'
+    t2 = '23/04/2018 || 1000.00 || || 2000.00'
+    t3 = '23/04/2018 || 2000.00 || || 1000.00'
+    statement = title + "\n" + t3 + "\n" + t2 + "\n" + t1
     it 'prints the bank statement' do
-      printer = Printer.new
-      title = 'date || credit || debit || balance'
-      t1 = '23/04/2018 || || 500.00 || 1500.00'
-      t2 = '23/04/2018 || 1000.00 || || 2000.00'
-      t3 = '23/04/2018 || 2000.00 || || 1000.00'
-      statement = title + "\n" + t3 + "\n" + t2 + "\n" + t1
       expect { printer.print(statement) }.to output(statement + "\n").to_stdout
+    end
+    it 'prints "hello"' do
+      expect { printer.print(statement) }.to_not output('hello').to_stdout
     end
   end
 end
